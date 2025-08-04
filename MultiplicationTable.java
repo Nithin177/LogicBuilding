12cccfca3095ab0624dbdec3f0b63818ac1a8e0a
NishaShetty7 <nishashetty102004@gmail.com>
2025-08-05 00:34:24 +0530

    second commit

---
+++ MultiplicationTable.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Multiplication table");
+        Scanner input= new Scanner(System.in);
+
+        System.out.println("Enter the Number You want me to generate the table");
+        int number= input.nextInt();
+        for( int i =1; i<=10;i++){
+            System.out.println(number + "*" + i + "= " + number*i );
+        }
+
+
+    }
+}
